332a614eccc404fd8bdc9bd9d9f0e02bc261d68c
jcpascual <julian.pascual@ontariotechu.net>
2024-03-07 17:21:15 -0500

    ConferenceUser: Add class to hold user information

---
+++ Conference/Models/ConferenceUser.cs
@@ -0,0 +1,22 @@
+namespace Conference.Models;
+
+public class ConferenceUser
+{
+    public int Id
+    {
+        get;
+        set;
+    }
+
+    public string Username
+    {
+        get;
+        set;
+    }
+
+    public string Password
+    {
+        get;
+        set;
+    }
+}
