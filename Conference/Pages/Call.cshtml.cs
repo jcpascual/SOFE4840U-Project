d05e98989d3b79b4898c03610006a1a7f5c545b3
jcpascual <julian.pascual@ontariotechu.net>
2024-03-18 20:14:13 -0400

    Call: Return NotFound if call doesn't exist

--- Conference/Pages/Call.cshtml.cs
+++ Conference/Pages/Call.cshtml.cs
@@ -1,3 +1,5 @@
+using Conference.Models;
+using Conference.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -7,15 +9,29 @@ namespace Conference.Pages;
 [Authorize]
 public class CallModel : PageModel
 {
+    private readonly CallCoordinatorService _coordinatorService;
+    
     public string CallId
     {
         get;
         set;
     } = "";
 
+    public CallModel(CallCoordinatorService coordinatorService)
+    {
+        _coordinatorService = coordinatorService;
+    }
+
     public IActionResult OnGet(string callId)
     {
         CallId = callId;
+
+        ConferenceCall? call = _coordinatorService.GetCall(callId);
+
+        if (call == null)
+        {
+            return NotFound();
+        }
         
         return Page();
     }
