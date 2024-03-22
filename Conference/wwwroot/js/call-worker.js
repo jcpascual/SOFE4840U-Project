8c0c789b3e51e31e9f11efca99fb50a3a068667c
jcpascual <julian.pascual@ontariotechu.net>
2024-03-22 01:09:56 -0400

    Call: Derive encryption key from password if specified

--- Conference/wwwroot/js/call-worker.js
+++ Conference/wwwroot/js/call-worker.js
@@ -1,44 +1,18 @@
-onmessage = ({data}) => {
+let callKey;
+
+self.onmessage = ({data}) => {
     if ("rtctransform" in data) {
         /* needed by chrome shim */
         onrtctransform({transformer: data.rtctransform});
+    } else if ("callkey" in data) {
+        callKey = data.callkey;
     }
 };
-
-async function getKey(roomId, password) {
-    const encoder = new TextEncoder();
-    
-    const keyMaterial = await crypto.subtle.importKey(
-        "raw",
-        encoder.encode(password),
-        "PBKDF2",
-        false,
-        ["deriveBits", "deriveKey"],
-    );
-    
-    const salt = await crypto.subtle.digest("SHA-256", encoder.encode(roomId));
-    
-    return crypto.subtle.deriveKey(
-        {
-            name: "PBKDF2",
-            hash: "SHA-256",
-            salt: salt,
-            iterations: 100000,
-        },
-        keyMaterial,
-        {
-            name: "AES-GCM", length: 256
-        },
-        true,
-        ["encrypt", "decrypt"],
-    );
-}
 
 async function encryptFrame(chunk, controller) {
     const firstFour = new Uint8Array(chunk.data, 0, 4);
     const everythingElse = new Uint8Array(chunk.data, 4);
     
-    const key = await getKey("1234567890", "password");
     const iv = crypto.getRandomValues(new Uint8Array(12));
     
     const ciphertext = await crypto.subtle.encrypt(
@@ -46,7 +20,7 @@ async function encryptFrame(chunk, controller) {
             name: "AES-GCM",
             iv: iv
         },
-        key,
+        callKey,
         everythingElse
     );
     
@@ -63,18 +37,17 @@ async function encryptFrame(chunk, controller) {
 }
 
 async function decryptFrame(chunk, controller) {
+    console.log("encrypt frame");
     const firstFour = new Uint8Array(chunk.data, 0, 4);
     const iv = new Uint8Array(chunk.data, 4, 12);
     const ciphertext = new Uint8Array(chunk.data, 16);
     
-    const key = await getKey("1234567890", "password");
-
     const plaintext = await crypto.subtle.decrypt(
         {
             name: "AES-GCM",
             iv: iv
         },
-        key,
+        callKey,
         ciphertext
     );
 
